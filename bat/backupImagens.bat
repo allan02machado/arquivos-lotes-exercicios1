bff608fde39b76ec62dd204cf197d5e2f0ea337b
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-10-14 15:30:56 -0300

    Exercicio backupImagens.bat

--- bat/backupImagens.bat
+++ bat/backupImagens.bat
@@ -2,7 +2,7 @@
 echo.
 echo Iremos realizar o backup de suas imagens em %1
 pause
-xcopy %allan%\Pictures %1 /d /s
-
-echo Arquivos copiados com sucesso.
+xcopy %userprofile%\Imagens %1 /d /s
+echo.
+echo Arquivos copiados com sucesso!
 pause
